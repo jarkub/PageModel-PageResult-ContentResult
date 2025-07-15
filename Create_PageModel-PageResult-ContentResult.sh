cbae4fe1dcb6250db29a8adef913e14b3f459a22
jaredk <jaredk@monnit.com>
2025-07-15 14:27:58 -0600

    initial commit

---
+++ Create_PageModel-PageResult-ContentResult.sh
@@ -0,0 +1,7 @@
+echo "# PageModel-PageResult-ContentResult" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jarkub/PageModel-PageResult-ContentResult.git
+git push -u origin main
